ec8b47b15bd1bd7a671ae73bfdc3781f3e39b2e8
Erika Heidi <erika@erikaheidi.com>
2024-10-03 20:08:36 +0200

    Updating Librarian docs look and feel, few other updates to docs

--- app/Resources/public/js/app.js
+++ app/Resources/public/js/app.js
@@ -2,9 +2,9 @@ window.onload = (event) => {
     const menu = document.querySelector('#blog-sidebar');
 
     let currentLinks = menu.querySelectorAll('a[href="' + window.location.pathname + '"]');
-    currentLinks.forEach(link => link.className += ' bg-gray-300 text-violet-900 font-bold');
+    currentLinks.forEach(link => link.className += ' text-violet-900 font-bold');
 
     let pathArray = window.location.pathname.split('/');
     let parentLinks = menu.querySelectorAll('a[href="/' + pathArray[1] + '"]');
-    parentLinks.forEach(link => link.className += ' border-l-8 border-teal-600');
+    parentLinks.forEach(link => link.className += ' border-l-8 border-purple-300');
 }
